d3d8b21950fdce2f629429653b708aedc1ff1fe0
Akihiro Urushihara <a.urusihara@gmail.com>
2026-05-13 17:11:28 +0900

    Expand UsersTest and NotificationsTest
    Add account, followers, following, statuses, and relationships tests
    to UsersTest.
    Add notification by ID, and subscription tests to NotificationsTest
    (with @Ignore on tests needing existing notification data).

--- core/src/jvmTest/kotlin/work/socialhub/kmastodon/apis/NotificationsTest.kt
+++ core/src/jvmTest/kotlin/work/socialhub/kmastodon/apis/NotificationsTest.kt
@@ -3,16 +3,39 @@ package work.socialhub.kmastodon.apis
 import kotlinx.coroutines.test.runTest
 import work.socialhub.kmastodon.AbstractTest
 import work.socialhub.kmastodon.Printer.dumpNotifications
+import work.socialhub.kmastodon.api.request.notifications.NotificationsNotificationRequest
 import work.socialhub.kmastodon.api.request.notifications.NotificationsNotificationsRequest
+import kotlin.test.Ignore
 import kotlin.test.Test
 
-class NotificationsTest : AbstractTest(){
+class NotificationsTest : AbstractTest() {
 
     @Test
     fun testNotifications() = runTest {
         val response = mastodon().notifications().notifications(
-           NotificationsNotificationsRequest()
+            NotificationsNotificationsRequest()
         )
         dumpNotifications(response.data)
+    }
+
+    @Test
+    @Ignore
+    fun testNotification() = runTest {
+        val notifications = mastodon().notifications().notifications(
+            NotificationsNotificationsRequest()
+        )
+        val firstId = notifications.data.firstOrNull()?.id ?: return@runTest
+
+        val response = mastodon().notifications().notification(
+            NotificationsNotificationRequest().also { it.id = firstId }
+        )
+        println("Notification type: ${response.data.type}")
+    }
+
+    @Test
+    @Ignore
+    fun testSubscription() = runTest {
+        val response = mastodon().notifications().subscription()
+        println("Subscription endpoint: ${response.data.endpoint}")
     }
 }
